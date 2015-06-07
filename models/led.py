33885b6f1509052ce91e2f969caea906d348fe6e
tauorus <tauorus@gmail.com>
2015-06-07 16:47:02 -0500

    no se usa libreria wiringpi2 por ahora

--- models/led.py
+++ models/led.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, exceptions
 from datetime import date
-import wiringpi2
+#import wiringpi2
+import subprocess
+from subprocess import CalledProcessError
 
 
 class led(models.Model):
@@ -20,8 +22,18 @@ class led(models.Model):
 
     @api.model
     def create(self, values):
-        wiringpi2.wiringPiSetupPhys() # init pin to phy mode,U14(1~48) U15(49~96)
-        wiringpi2.pinMode(self.led_pin,1) # set pin 17 to output mode 1
+        export_= '/sys/class/gpio/export'
+        _cwd = '/sys/class/gpio'
+        try:
+            subprocess.check_output(['echo', led_pin, '>', fexport_], stderr=subprocess.STDOUT, cwd=_cwd)
+        except:
+            print('Hubo un problema habilitando el pin.')
+        try:
+            subprocess.check_output(['echo', 'out', '>', 'gpio{}_pg*/direction'.format(led_pin)], stderr=subprocess.STDOUT, cwd=_cwd)
+        except:
+            raise exceptions.Warning('Hubo un problema configurando el pin como salida.')
+#         wiringpi2.wiringPiSetupPhys() # init pin to phy mode,U14(1~48) U15(49~96)
+#         wiringpi2.pinMode(self.led_pin,1) # set pin 17 to output mode 1
         return super(led, self).create(values)
 
 
@@ -29,15 +41,25 @@ class led(models.Model):
     def action_prender(self):
         self.state = 'prendido'
         self.date = date.today()
-        wiringpi2.digitalWrite(self.led_pin,1) # Write 1 HIGH to pin 2
-        if wiringpi2.digitalRead(self.led_pin) != 1:
-            raise exceptions.Warning('Hubo un problema encendiando el LED.')
+#         wiringpi2.digitalWrite(self.led_pin,1) # Write 1 HIGH to pin 2
+        _cwd = '/sys/class/gpio'
+        try:
+            subprocess.check_output(['echo', '1', '>', 'gpio{}_pg*/value'.format(led_pin)], stderr=subprocess.STDOUT, cwd=_cwd)
+        except:
+            print('Hubo un problema prendiendo el LED.')
+#         if wiringpi2.digitalRead(self.led_pin) != 1:
+#             raise exceptions.Warning('Hubo un problema encendiando el LED.')
 
 
     @api.one
     def action_apagar(self):
         self.state = 'apagado'
         self.date = date.today()
-        wiringpi2.digitalWrite(self.led_pin,0) # Write 0 to pin 17
-        if wiringpi2.digitalRead(self.led_pin) != 0:
-            raise exceptions.Warning('Hubo un problema apagando el LED.')
+#         wiringpi2.digitalWrite(self.led_pin,0) # Write 0 to pin 17
+        _cwd = '/sys/class/gpio'
+        try:
+            subprocess.check_output(['echo', '0', '>', 'gpio{}_pg*/value'.format(led_pin)], stderr=subprocess.STDOUT, cwd=_cwd)
+        except:
+            print('Hubo un problema apagando el LED.')
+#         if wiringpi2.digitalRead(self.led_pin) != 0:
+#             raise exceptions.Warning('Hubo un problema apagando el LED.')
